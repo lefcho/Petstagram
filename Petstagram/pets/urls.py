2da9894174b6ccff229e5f19df808c6f67482185
ivand <ivan.d.kostov@gmail.com>
2024-10-19 23:37:09 +0300

    updated: pet views from FBV to CBV

--- Petstagram/pets/urls.py
+++ Petstagram/pets/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from Petstagram.pets import views
 
 urlpatterns = [
-    path('add/', views.add_pet, name='add-pet'),
+    path('add/', views.AddPetView.as_view(), name='add-pet'),
     path('<str:username>/pet/<slug:pet_slug>/', include([
-        path('', views.details_pet, name='details_pet'),
-        path('edit/', views.edit_pet, name='edit_pet'),
-        path('delete/', views.delete_pet, name='delete_pet'),
+        path('', views.DetailsPetView.as_view(), name='details_pet'),
+        path('edit/', views.EditPetView.as_view(), name='edit_pet'),
+        path('delete/', views.DeletePetView.as_view(), name='delete_pet'),
     ]))
 ]
--- Petstagram/pets/views.py
+++ Petstagram/pets/views.py
@@ -1,64 +1,63 @@
-from django.shortcuts import render, redirect
 
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+
+from Petstagram.common.forms import AddCommentForm
 from Petstagram.pets.forms import AddPetForm, EditPetForm, DeletePetForm
 from Petstagram.pets.models import Pet
 
 
-def add_pet(request):
-    form = AddPetForm(request.POST or None)
-
-    if form.is_valid():
-        form.save()
-        return redirect('profile-details', pk=1)
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'pets/pet-add-page.html', context)
+class AddPetView(CreateView):
+    model = Pet
+    template_name = 'pets/pet-add-page.html'
+    form_class = AddPetForm
+    success_url = reverse_lazy('profile-details', kwargs={'pk': 1})
 
 
-def details_pet(request, username, pet_slug):
-    pet = Pet.objects.get(slug=pet_slug)
-    pet_photos = pet.photos.all()
+class DetailsPetView(DetailView):
+    model = Pet
+    template_name = 'pets/pet-details-page.html'
+    context_object_name = 'pet'
+    slug_url_kwarg = 'pet_slug'
 
-    context = {
-        'pet': pet,
-        'pet_photos': pet_photos,
-    }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pet_photos'] = self.object.photos.all()
+        context['comment_form'] = AddCommentForm()
 
-    return render(request, 'pets/pet-details-page.html', context)
-
-
-def edit_pet(request, username, pet_slug):
-    pet = Pet.objects.get(slug=pet_slug)
-    form = EditPetForm(request.POST or None, instance=pet)
-
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('details_pet', 'username', pet.slug)
-
-    context = {
-        'form': form,
-        'pet': pet,
-    }
-
-    return render(request, 'pets/pet-edit-page.html', context)
+        return context
 
 
-def delete_pet(request, username ,pet_slug):
-    pet = Pet.objects.get(slug=pet_slug)
-    form = DeletePetForm(instance=pet)
+class EditPetView(UpdateView):
+    model = Pet
+    template_name = 'pets/pet-edit-page.html'
+    form_class = EditPetForm
+    slug_url_kwarg = 'pet_slug'
 
-    if request.method == 'POST':
-        pet.delete()
+    def get_success_url(self):
+        return reverse_lazy(
+            'details_pet',
+            kwargs={
+                'username': self.kwargs['username'],
+                'pet_slug': self.kwargs['pet_slug']
+            }
+        )
 
-        return redirect('profile-details', pk=1)
 
-    context = {
-        'form': form,
-        'pet': pet,
-    }
+class DeletePetView(DeleteView):
+    model = Pet
+    template_name = 'pets/pet-delete-page.html'
+    form_class = DeletePetForm
+    success_url = reverse_lazy('profile-details', kwargs={'pk': 1})
+    slug_url_kwarg = 'pet_slug'
 
-    return render(request, 'pets/pet-delete-page.html', context)
+    def get_initial(self):
+        return self.get_object().__dict__
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({
+            'data': self.get_initial(),
+        })
+
+        return kwargs
